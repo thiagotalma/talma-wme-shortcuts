6c48f6b307d5350d022097d5a6f51afd9659accc
Thiago Talma <thiago@thiagomt.com>
2015-02-03 17:55:17 -0200

    ajustes

--- testesUnit.js
+++ testesUnit.js
@@ -1,4 +1,10 @@
 var testesUnit = [
-    ["Rua Vereador Jose Antonio", "R. Ver. José Antônio"],
-    ["Avenida Doutora Nossa Senhora Das Graças", "Av. Dra. N.Sra. das Graças"]
+    ["Rua Vereador Jose Antonio", "R. Ver. José Antônio"]
+    , ["Avenida Doutora Nossa Senhora Das Graças", "Av. Dra. N.Sra. das Graças"]
+    , ["Praça Álvaro Antonio", "Pç. Álvaro Antônio"]
+    , ["Rua Dona Olivia Bagatin De Freitas", "R. Da. Olivia Bagatin de Freitas"]
+    , ["Viela Miranda", "Ve. Miranda"]
+    , ["Ve Miranda De Ágape", "Ve. Miranda de Ágape"]
+    , ["Travessa Teste", "Tv. Teste"]
+    , ["TV Teste", "Tv. Teste"]
 ];
